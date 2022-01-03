d88d73a6cd24169450c1252dc4c0b1f201602ca4
akash172891 <76027562+akash172891@users.noreply.github.com>
2022-01-03 15:43:30 +0530

    Create gcd of strings.cpp

---
+++ gcd of strings.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        string ans="";
+        if(str1+str2!=str2+str1) return ans;
+        
+        ans+= str1.substr(0,gcd(str1.size(),str2.size()));
+        
+        return ans;
+    }
+};
